5455c6c0ea4d45aef55f8d4d899080b551c9abc8
kirakiray <pikay@foxmail.com>
2019-09-22 22:33:19 +0800

    backup

--- dist/xdframe.js
+++ dist/xdframe.js
@@ -1866,9 +1866,6 @@
         }
 
         const isXhear = (target) => target instanceof XhearEle;
-        // 可setData的key
-        const CANSETKEYS = Symbol("cansetkeys");
-        const ORIEVE = Symbol("orignEvents");
 
         // 将 element attribute 横杠转换为大小写模式
         const attrToProp = key => {
@@ -1884,6 +1881,9 @@
             }
             return key;
         }
+        // 可setData的key
+        const CANSETKEYS = Symbol("cansetkeys");
+        const ORIEVE = Symbol("orignEvents");
 
         // 可直接设置的Key
         const xEleDefaultSetKeys = new Set(["text", "html", "display", "style"]);
@@ -2852,6 +2852,7 @@
                         value
                     } = obj;
                     let prop = value;
+                    name = attrToProp(name);
 
                     let matchArr = /^:(.+)/.exec(name);
                     if (matchArr) {
@@ -3029,6 +3030,7 @@
                     let {
                         name
                     } = e;
+                    name = attrToProp(name);
                     if (!/^xv\-/.test(name) && !/^:/.test(name) && canSetKey.has(name)) {
                         rData[name] = e.value;
                     }
