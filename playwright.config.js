b4d7c9548841ae61661412a0edd9bd1b618abe93
huang yao <kirayao@hotmail.com>
2023-06-23 14:15:26 +0800

    test: add o-router test case

--- playwright.config.js
+++ playwright.config.js
@@ -11,7 +11,8 @@ const { defineConfig, devices } = require('@playwright/test');
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  testDir: './test',
+  testDir: "./",
+  testMatch: ["**/*.spec.js"],
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
