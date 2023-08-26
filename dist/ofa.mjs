2d521bd23b05ec556cb6ad6781b1b730d28d8e1e
Yao <kirayao@hotmail.com>
2023-08-26 19:23:01 +0800

    fix: update xhear (#41)
    * fix: update xhear
    
    * chore: bump version
    
    ---------
    
    Co-authored-by: huang yao <5945154+kirakiray@users.noreply.github.com>

--- dist/ofa.mjs
+++ dist/ofa.mjs
@@ -1,4 +1,4 @@
-//! ofa.js - v4.2.4 https://github.com/kirakiray/ofa.js  (c) 2018-2023 YAO
+//! ofa.js - v4.2.5 https://github.com/kirakiray/ofa.js  (c) 2018-2023 YAO
 const getRandomId = () => Math.random().toString(32).slice(2);
 
 const objectToString = Object.prototype.toString;
@@ -2405,6 +2405,10 @@ register({
       }
 
       const regData = getRenderData(this._fake);
+
+      if (!regData) {
+        return;
+      }
 
       const xids = childs.map((e) => e._data_xid);
 
