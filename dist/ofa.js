25145e923ff56ae04ca98b4e4052bdb50dd33221
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-10-19 14:59:28 +0800

    fix: update xhear

--- dist/ofa.js
+++ dist/ofa.js
@@ -1336,18 +1336,24 @@ try{
     e.result();
   };
 
-  const eventFn = {
-    on(name, func, options) {
-      let revoker;
-      if (options) {
-        const beforeValue = options.beforeArgs[1];
+  function getBindOptions(name, func, options) {
+    let revoker;
+    if (options) {
+      const beforeValue = options.beforeArgs[1];
 
-        if (!/[^\d\w_\$\.]/.test(beforeValue)) {
-          func = options.data.get(beforeValue).bind(options.data);
-        }
-
-        revoker = () => this.ele.removeEventListener(name, func);
+      if (!/[^\d\w_\$\.]/.test(beforeValue)) {
+        func = options.data.get(beforeValue).bind(options.data);
       }
+
+      revoker = () => this.ele.removeEventListener(name, func);
+    }
+
+    return { revoker, name, func };
+  }
+
+  const eventFn = {
+    on(...args) {
+      const { revoker, name, func } = getBindOptions.call(this, ...args);
 
       this.ele.addEventListener(name, func);
 
@@ -1357,13 +1363,19 @@ try{
 
       return this;
     },
-    one(name, func, options) {
-      const callback = (e) => {
+    one(...args) {
+      const { revoker, name, func } = getBindOptions.call(this, ...args);
+
+      let callback = (e) => {
         this.off(name, callback);
         func(e);
       };
 
-      this.on(name, callback, options);
+      this.ele.addEventListener(name, callback);
+
+      if (revoker) {
+        return revoker;
+      }
 
       return this;
     },
@@ -1397,6 +1409,10 @@ try{
   };
 
   eventFn.on.revoke = (e) => {
+    e.result();
+  };
+
+  eventFn.one.revoke = (e) => {
     e.result();
   };
 
