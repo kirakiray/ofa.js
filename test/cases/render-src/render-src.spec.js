e9bba1846094af430065521233bc5b1a62b9c20c
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-11-14 16:10:47 +0800

    Update render-src.spec.js

--- test/cases/render-src/render-src.spec.js
+++ test/cases/render-src/render-src.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("test", async ({ page }) => {
+test("render page src", async ({ page }) => {
   await page.goto("http://localhost:3348/test/cases/render-src/test.html");
   await page.getByText("Page 1").click();
   await page.getByTestId("link1").click();
