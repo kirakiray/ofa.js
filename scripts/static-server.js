7ee67257d6f5551e7717e303aadf903da1fa424c
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-01-06 16:09:41 +0800

    refactor: add cors case

--- scripts/static-server.js
+++ scripts/static-server.js
@@ -24,7 +24,25 @@ app.use(home);
 const _server = app.listen(3348);
 console.log(`server start => http://localhost:3348/`);
 
-const _server2 = app.listen(33482);
+const app2 = new Koa();
+const home2 = serve(path.normalize(__dirname + "/../test/"));
+app2.use(async (ctx, next) => {
+  ctx.set("Access-Control-Allow-Origin", "*");
+  ctx.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Content-Length, Authorization, Accept, X-Requested-With"
+  );
+  ctx.set("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  if (ctx.method == "OPTIONS") {
+    ctx.body = 200;
+  } else {
+    await next();
+  }
+});
+
+app2.use(home2);
+
+const _server2 = app2.listen(33482);
 
 module.exports = {
   server: _server,
