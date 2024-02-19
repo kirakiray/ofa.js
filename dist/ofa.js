7947d4e815411cb202dc1637aa5715a08ac628fb
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-02-19 17:59:12 +0800

    chore: bump version

--- dist/ofa.js
+++ dist/ofa.js
@@ -1,4 +1,4 @@
-//! ofa.js - v4.4.5 https://github.com/kirakiray/ofa.js  (c) 2018-2024 YAO
+//! ofa.js - v4.4.6 https://github.com/kirakiray/ofa.js  (c) 2018-2024 YAO
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
@@ -5462,7 +5462,7 @@ ${scriptContent}`;
     attr,
   });
 
-  const version = "ofa.js@4.4.5";
+  const version = "ofa.js@4.4.6";
   $.version = version.replace("ofa.js@", "");
 
   if (document.currentScript) {
