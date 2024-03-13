49c501c7342595ef2c75d8c0b3c1a01369fe7e0e
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-03-13 23:04:18 +0800

    fix: scsr mode not passing search parameters

--- dist/ofa.mjs
+++ dist/ofa.mjs
@@ -1,4 +1,4 @@
-//! ofa.js - v4.4.8 https://github.com/kirakiray/ofa.js  (c) 2018-2024 YAO
+//! ofa.js - v4.4.9 https://github.com/kirakiray/ofa.js  (c) 2018-2024 YAO
 const getRandomId = () => Math.random().toString(32).slice(2);
 
 const objectToString = Object.prototype.toString;
@@ -5489,7 +5489,7 @@ $.fn.extend({
   attr,
 });
 
-const version = "ofa.js@4.4.8";
+const version = "ofa.js@4.4.9";
 $.version = version.replace("ofa.js@", "");
 
 if (document.currentScript) {
