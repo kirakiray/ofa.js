b1b872358c7a1e2695a0f3e294fd068470b5b2f8
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-03-20 00:22:56 +0800

    chore: bump version

--- dist/ofa.js
+++ dist/ofa.js
@@ -1,4 +1,4 @@
-//! ofa.js - v4.4.9 https://github.com/kirakiray/ofa.js  (c) 2018-2024 YAO
+//! ofa.js - v4.4.10 https://github.com/kirakiray/ofa.js  (c) 2018-2024 YAO
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
@@ -957,6 +957,49 @@ try{
     const tasks = [];
     const revokes = getRevokes(target);
 
+    // Styles with data() function to monitor and correct rendering
+    searchEle(target, "style").forEach((el) => {
+      const originStyle = el.innerHTML;
+
+      if (/data\(.+\)/.test(originStyle)) {
+        const matchs = Array.from(new Set(originStyle.match(/data\(.+?\)/g))).map(
+          (dataExpr) => {
+            const expr = dataExpr.replace(/data\((.+)\)/, "$1");
+            const func = convertToFunc(expr, data);
+
+            return {
+              dataExpr,
+              func,
+            };
+          }
+        );
+
+        const renderStyle = () => {
+          let afterStyle = originStyle;
+
+          matchs.forEach(({ dataExpr, func }) => {
+            afterStyle = afterStyle.replace(dataExpr, func());
+          });
+
+          if (el.innerHTML !== afterStyle) {
+            el.innerHTML = afterStyle;
+          }
+        };
+        tasks.push(renderStyle);
+
+        const revokeStyle = () => {
+          matchs.length = 0;
+          removeArrayValue(tasks, renderStyle);
+          removeArrayValue(getRevokes(el), revokeStyle);
+          removeArrayValue(revokes, revokeStyle);
+        };
+
+        addRevoke(el, revokeStyle);
+        revokes.push(revokeStyle);
+      }
+    });
+
+    // Render text nodes
     texts.forEach((el) => {
       const textEl = document.createTextNode("");
       const { parentNode } = el;
@@ -987,6 +1030,7 @@ try{
       eles.unshift(target);
     }
 
+    // Render properties based on expressions
     eles.forEach((el) => {
       const bindData = JSON.parse(el.getAttribute("x-bind-data"));
 
@@ -1105,6 +1149,7 @@ try{
 
       tasks.forEach((f) => f());
 
+      // After the data changes, traverse the rendering tasks
       const wid = data.watchTick((e) => {
         if (tasks.length) {
           tasks.forEach((f) => f());
@@ -5495,7 +5540,7 @@ ${scriptContent}`;
     attr,
   });
 
-  const version = "ofa.js@4.4.9";
+  const version = "ofa.js@4.4.10";
   $.version = version.replace("ofa.js@", "");
 
   if (document.currentScript) {
