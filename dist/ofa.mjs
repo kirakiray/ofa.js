bbc5dfc50f0bd4d34f5dd01c4299a56d14d6363b
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-04-11 21:36:56 +0800

    chore: set dev version

--- dist/ofa.mjs
+++ dist/ofa.mjs
@@ -1,4 +1,4 @@
-//! ofa.js - v4.4.13 https://github.com/kirakiray/ofa.js  (c) 2018-2024 YAO
+//! ofa.js - v4.4.12.2 https://github.com/kirakiray/ofa.js  (c) 2018-2024 YAO
 const getRandomId = () => Math.random().toString(32).slice(2);
 
 const objectToString = Object.prototype.toString;
