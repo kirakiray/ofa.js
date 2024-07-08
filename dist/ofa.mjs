a0fcc77cbaeb808e627f0a16af3cdc4d929137ac
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-07-08 21:52:25 +0800

    chore: bump version

--- dist/ofa.mjs
+++ dist/ofa.mjs
@@ -1,4 +1,4 @@
-//! ofa.js - v4.4.16 https://github.com/kirakiray/ofa.js  (c) 2018-2024 YAO
+//! ofa.js - v4.5.0 https://github.com/kirakiray/ofa.js  (c) 2018-2024 YAO
 // const error_origin = "http://127.0.0.1:5793/errors";
 const error_origin = "https://ofajs.github.io/ofa-errors/errors";
 
@@ -6070,7 +6070,7 @@ $.register({
   },
 });
 
-const version = "ofa.js@4.4.16";
+const version = "ofa.js@4.5.0";
 $.version = version.replace("ofa.js@", "");
 
 if (document.currentScript) {
