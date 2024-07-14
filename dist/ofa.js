36a787447151590a1346c99e7f08c0831cebe6f7
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-07-14 15:21:21 +0800

    chroe: bump version

--- dist/ofa.js
+++ dist/ofa.js
@@ -1,4 +1,4 @@
-//! ofa.js - v4.5.0 https://github.com/kirakiray/ofa.js  (c) 2018-2024 YAO
+//! ofa.js - v4.5.1 https://github.com/kirakiray/ofa.js  (c) 2018-2024 YAO
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
@@ -6286,7 +6286,7 @@ ${scriptContent}`;
     },
   });
 
-  const version = "ofa.js@4.5.0";
+  const version = "ofa.js@4.5.1";
   $.version = version.replace("ofa.js@", "");
 
   if (document.currentScript) {
