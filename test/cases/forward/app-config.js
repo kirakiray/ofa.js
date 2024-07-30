7b4d0373151fe705f34e36bb5a148398bc4bf269
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-07-30 16:55:07 +0800

    test: add forward case

--- test/cases/forward/app-config.js
+++ test/cases/forward/app-config.js
@@ -1,17 +1,17 @@
-export const pageAnime = {
-  current: {
-    opacity: 1,
-    transform: "translate(0, 0)",
-  },
-  next: {
-    opacity: 0,
-    transform: "translate(30px, 0)",
-  },
-  previous: {
-    opacity: 0,
-    transform: "translate(-30px, 0)",
-  },
-};
+// export const pageAnime = {
+//   current: {
+//     opacity: 1,
+//     transform: "translate(0, 0)",
+//   },
+//   next: {
+//     opacity: 0,
+//     transform: "translate(30px, 0)",
+//   },
+//   previous: {
+//     opacity: 0,
+//     transform: "translate(-30px, 0)",
+//   },
+// };
 
 export const home = "./p1.html";
 
