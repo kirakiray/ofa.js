8632dae3cfe01506afece47cd91eb14640799358
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-08-21 23:07:00 +0800

    feat: backup body ghost code

--- libs/body-ghost/body-ghost.js
+++ libs/body-ghost/body-ghost.js
@@ -1,63 +1,72 @@
-(() => {
-  const MAPPGING_ELE = Symbol("mapping-element");
-  const ORIGIN_ELE = Symbol("origin-element");
+// import { revokeAll } from "../../packages/xhear/util.mjs";
 
-  $.register({
-    tag: "body-ghost-mapping",
-    temp: `<style>:host{display:contents}</style>`,
-  });
+const MAPPGING_ELE = Symbol("mapping-element");
+const ORIGIN_ELE = Symbol("origin-element");
 
-  $.register({
-    tag: "body-ghost",
-    data: {
-      ghostId: Math.random().toString(32).slice(2),
+$.register({
+  tag: "body-ghost-mapping",
+  temp: `<style>:host{display:contents}</style>`,
+});
+
+$.register({
+  tag: "body-ghost",
+  data: {
+    ghostId: Math.random().toString(32).slice(2),
+  },
+  temp: `<style>:host{display:none}</style>`,
+  proto: {
+    get mapping() {
+      return this[MAPPGING_ELE];
     },
-    temp: `<style>:host{display:none}</style>`,
-    proto: {
-      get mapping() {
-        return this[MAPPGING_ELE];
+  },
+  ready() {
+    this[MAPPGING_ELE] = null;
+  },
+  attached() {
+    const mappingEl = (this[MAPPGING_ELE] =
+      document.createElement("body-ghost-mapping"));
+
+    mappingEl[ORIGIN_ELE] = this.ele;
+
+    Object.defineProperties(mappingEl.shadowRoot, {
+      // 伪装parentNode，让所有事情向上冒泡
+      parentNode: {
+        get: () => {
+          return this.ele.parentNode;
+        },
       },
-    },
-    ready() {
+    });
+
+    const originChildNodes = this.ele.childNodes;
+
+    Object.defineProperties(this.ele, {
+      // 伪装子元素
+      childNodes: {
+        get: () => {
+          return mappingEl.shadowRoot.childNodes;
+        },
+      },
+    });
+
+    $.nextTick(() => {
+      Array.from(originChildNodes).forEach((el) => {
+        el.__internal = 1;
+        mappingEl.shadowRoot.appendChild(el);
+        delete el.__internal;
+      });
+    });
+
+    document.body.appendChild(mappingEl);
+  },
+  detached() {
+    if (this[MAPPGING_ELE]) {
+      const oldMapping = this[MAPPGING_ELE];
+      this[MAPPGING_ELE].remove();
       this[MAPPGING_ELE] = null;
-    },
-    attached() {
-      const mappingEl = (this[MAPPGING_ELE] =
-        document.createElement("body-ghost-mapping"));
 
-      mappingEl[ORIGIN_ELE] = this.ele;
-
-      const originChildNodes = this.ele.childNodes;
-
-      // 伪装 childNodes
-      Object.defineProperties(this.ele, {
-        childNodes: {
-          get: () => {
-            return this[MAPPGING_ELE].childNodes;
-          },
-        },
-        _childNodes: {
-          get() {
-            return originChildNodes;
-          },
-        },
-      });
-
-      $.nextTick(() => {
-        Array.from(originChildNodes).forEach((el) => {
-          el.__internal = 1;
-          mappingEl.shadowRoot.appendChild(el);
-          delete el.__internal;
-        });
-      });
-
-      document.body.appendChild(mappingEl);
-    },
-    detached() {
-      if (this[MAPPGING_ELE]) {
-        this[MAPPGING_ELE].remove();
-        this[MAPPGING_ELE] = null;
-      }
-    },
-  });
-})();
+      // 回收旧内容
+      //   revokeAll(oldMapping.);
+      //   debugger;
+    }
+  },
+});
