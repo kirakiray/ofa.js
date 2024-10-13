1550254375eeec09cece9828b0c6b8504001e7c9
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-10-13 16:27:15 +0800

    feat: Component module can get url

--- dist/ofa.js
+++ dist/ofa.js
@@ -1,4 +1,4 @@
-//! ofa.js - v4.5.20 https://github.com/kirakiray/ofa.js  (c) 2018-2024 YAO
+//! ofa.js - v4.5.21 https://github.com/kirakiray/ofa.js  (c) 2018-2024 YAO
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
@@ -5481,6 +5481,7 @@ ${scriptContent}`;
         load: lm$1({
           url: path,
         }),
+        url: path,
       });
     } else if (defaultData instanceof Object) {
       finnalDefault = { ...defaultData };
@@ -6563,7 +6564,7 @@ ${scriptContent}`;
     },
   });
 
-  const version = "ofa.js@4.5.20";
+  const version = "ofa.js@4.5.21";
   $.version = version.replace("ofa.js@", "");
 
   if (document.currentScript) {
