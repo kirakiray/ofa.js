c2e5faaf5dbee3357a01cb33257592857f2f35c2
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-05-06 17:07:13 +0800

    Create if.spec.js

---
+++ test/cases/o-if/if.spec.js
@@ -0,0 +1,30 @@
+import { test, expect } from "@playwright/test";
+
+test("match var", async ({ page }) => {
+  await page.goto("http://localhost:3348/test/cases/o-if/demo.html");
+
+  // 断言文本是否包含 'I am 0 green'
+  expect(await page.$eval("#tips", (el) => el.textContent)).toContain(
+    "I am 0 green"
+  );
+
+  await page.getByRole("button", { name: "+1" }).click();
+
+  expect(await page.$eval("#tips", (el) => el.textContent)).toContain(
+    "I am 1 pink"
+  );
+
+  await page.getByRole("button", { name: "+1" }).click();
+
+  expect(await page.$eval("#tips", (el) => el.textContent)).toContain(
+    "I am 2 red"
+  );
+
+  await page.getByRole("button", { name: "-1" }).click();
+  await page.getByRole("button", { name: "-1" }).click();
+  await page.getByRole("button", { name: "-1" }).click();
+
+  expect(await page.$eval("#tips", (el) => el.textContent)).toContain(
+    "I am -1 blue"
+  );
+});
