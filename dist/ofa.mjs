239ba7517b7f14cec44a195d404641abce82256b
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-05-07 14:11:58 +0800

    chore: build project

--- dist/ofa.mjs
+++ dist/ofa.mjs
@@ -3945,12 +3945,12 @@ const objToXEle = (obj) => {
   return $ele;
 };
 
-const temp$1 = document.createElement("template");
+const temp$2 = document.createElement("template");
 
 const strToXEle = (str) => {
-  temp$1.innerHTML = str;
-  const ele = temp$1.content.children[0] || temp$1.content.childNodes[0];
-  temp$1.innerHTML = "";
+  temp$2.innerHTML = str;
+  const ele = temp$2.content.children[0] || temp$2.content.childNodes[0];
+  temp$2.innerHTML = "";
 
   return eleX(ele);
 };
@@ -6289,7 +6289,7 @@ $.fn.extend({
 // 根provider
 const rootProviders = {};
 
-const temp = `<style>:host{display:contents}</style><slot></slot>`;
+const temp$1 = `<style>:host{display:contents}</style><slot></slot>`;
 
 const CONSUMERS = Symbol("consumers");
 const PROVIDER = Symbol("provider");
@@ -6422,7 +6422,7 @@ const InvalidKeys = [
 
 const providerOptions = {
   tag: "o-provider",
-  temp,
+  temp: temp$1,
   attrs: {
     name: null,
   },
@@ -6655,7 +6655,7 @@ const emitAllConsumer = (ele, rootProvider, emitSlot = true) => {
 
 $.register({
   tag: "o-consumer",
-  temp,
+  temp: temp$1,
   attrs: {
     name: null,
   },
@@ -6957,11 +6957,13 @@ const refreshCondition = (conditionEl) => {
       // 如果找到目标，就渲染目标元素
       if (targetCondition) {
         renderContent(targetCondition);
+        targetCondition.attr("actived", "");
       }
 
       // 清空其他元素内容
       conditionEls.forEach((el) => {
         if (el !== targetCondition) {
+          el.attr("actived", null);
           clearContent(el);
         }
       });
@@ -7039,8 +7041,11 @@ const createdFunc = function (_this) {
   _this.html = "";
 };
 
+const temp = `<style>:host{display:contents;}</style><slot></slot>`;
+
 $.register({
   tag: "o-if",
+  temp,
   data: {
     value: null,
   },
@@ -7056,6 +7061,7 @@ $.register({
 
 $.register({
   tag: "o-else-if",
+  temp,
   data: {
     value: null,
   },
@@ -7071,8 +7077,189 @@ $.register({
 
 $.register({
   tag: "o-else",
+  temp,
   created() {
     createdFunc(this);
+  },
+});
+
+$.register({
+  tag: "o-fill",
+  temp: `<style>:host{display:contents;}</style><slot></slot>`,
+  data: {
+    value: null,
+  },
+  proto: {
+    refreshView() {
+      const arr = this.value;
+      const tempName = this.attr("name");
+
+      if (
+        !arr ||
+        !arr.length ||
+        (this.__oldTempName && this.__oldTempName !== tempName)
+      ) {
+        // 没有值，清空内容
+        Array.from(this.ele.childNodes).forEach((e) => {
+          revokeAll(e);
+          e.remove();
+        });
+        return;
+      }
+
+      const { data, target, temps } = getRenderData(this.ele);
+
+      const keyName = this.attr("fill-key") || "xid";
+
+      let targetTemp = temps[tempName];
+
+      if (!targetTemp) {
+        // 没有找到模板，查看是否有默认的模板
+        if (this.__originHTML) {
+          targetTemp = $(`<template>${this.__originHTML}</template>`).ele;
+        } else {
+          throw new Error("o-fill - Template not found: " + tempName);
+        }
+      }
+
+      this.__oldTempName = tempName;
+
+      if (!this.length) {
+        // 没有子元素，优化性能的添加方式
+        const frag = document.createDocumentFragment();
+
+        // 渲染模板
+        for (let i = 0, len = arr.length; i < len; i++) {
+          const item = arr[i];
+
+          const $ele = createItem(
+            item,
+            temps,
+            targetTemp,
+            data.$host || data,
+            i,
+            keyName
+          );
+
+          frag.appendChild($ele.ele);
+        }
+
+        this.ele.appendChild(frag);
+        return;
+      }
+
+      // 有子元素，优化性能的方式更新方式
+      const keyValsArr = arr.map((e) => e[keyName]);
+
+      // 先删除不存在的元素
+      for (let e of Array.from(this.ele.children)) {
+        const renderedItem = e.__render_data;
+
+        const currentKeyVal = renderedItem.$data[keyName];
+
+        // 不存在的id，需要删除
+        if (!keyValsArr.includes(currentKeyVal)) {
+          e.remove();
+          revokeAll(e);
+        }
+      }
+
+      const { children } = this.ele;
+
+      // 获取当前所有的key值
+      let keyVals = [];
+      const refreshKeyVals = () => {
+        keyVals = Array.from(children).map(
+          (e) => e.__render_data.$data[keyName]
+        );
+      };
+      refreshKeyVals();
+
+      const selfEl = this.ele;
+
+      // 遍历一遍进行更新数据
+      for (let i = 0, len = arr.length; i < len; i++) {
+        const item = arr[i];
+        const keyVal = item[keyName];
+
+        // 查找是否存在
+        const index = keyVals.indexOf(keyVal);
+
+        if (index > -1) {
+          // 存在，更新数据
+          const $ele = eleX(children[index]);
+
+          // 如果不是目标元素，则进行位置调整
+          if (children[i] !== $ele.ele) {
+            selfEl.insertBefore($ele.ele, children[i]);
+            refreshKeyVals();
+          }
+
+          // 按需更新绑定的数据
+          if ($ele.__item.$data !== item) {
+            $ele.__item.$data = item;
+          }
+          if ($ele.__item.$index !== i) {
+            $ele.__item.$index = i;
+          }
+        } else {
+          // 不存在，添加新的元素
+          const $ele = createItem(
+            item,
+            temps,
+            targetTemp,
+            data.$host || data,
+            i,
+            keyName
+          );
+
+          selfEl.insertBefore($ele.ele, children[i]);
+          refreshKeyVals();
+        }
+      }
+    },
+  },
+  ready() {
+    this.watchTick((e) => {
+      if (e.hasModified("value")) {
+        // value发生变动，重新渲染
+        this.refreshView();
+      }
+    });
+  },
+  attached() {
+    if (this.value) {
+      this.refreshView();
+    }
+  },
+  created() {
+    // 检查内部是否包含无name的o-fill
+    const fillEls = this.all("o-fill:not([name])");
+
+    if (fillEls.length > 0) {
+      const err = getErr("xhear_dbfill_noname");
+      console.warn(err, this.ele, {
+        content: this.html.trim(),
+      });
+      fillEls.forEach((e) => {
+        e.remove();
+      });
+      throw err;
+    }
+
+    if (this.length > 1 || (this.length === 0 && this.html.trim())) {
+      const err = getErr("temp_multi_child");
+      console.warn(err, this.ele, {
+        content: this.html.trim(),
+      });
+
+      this.__originHTML = `<div style="display: contents;">${this.html}</div>`;
+    } else {
+      // 创建的时候，将内容抽取成模板
+      this.__originHTML = this.html.trim();
+    }
+
+    this.html = "";
   },
 });
 
--- test/cases/o-if/if.spec.js
+++ test/cases/o-if/if.spec.js
@@ -3,6 +3,8 @@ import { test, expect } from "@playwright/test";
 test("match var", async ({ page }) => {
   await page.goto("http://localhost:3348/test/cases/o-if/demo.html");
 
+  await page.waitForSelector("#tips");
+
   // 断言文本是否包含 'I am 0 green'
   expect(await page.$eval("#tips", (el) => el.textContent)).toContain(
     "I am 0 green"
