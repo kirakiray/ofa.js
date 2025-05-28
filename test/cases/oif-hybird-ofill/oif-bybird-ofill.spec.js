11e9db13aaaa4cbd18160501183cfec57999b8d6
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-05-28 11:53:04 +0800

    fix(fill): 优化o-fill组件中key值的处理逻辑

--- test/cases/oif-hybird-ofill/oif-bybird-ofill.spec.js
+++ test/cases/oif-hybird-ofill/oif-bybird-ofill.spec.js
@@ -52,3 +52,27 @@ count: 2 yellow`);
   await expect(page.locator("c-ifinif")).toContainText(`{"count":3}
 count: 3 blue`);
 });
+
+test("o-fill in o-if", async ({ page }) => {
+  await page.goto(
+    "http://localhost:3348/test/cases/oif-hybird-ofill/demo3.html"
+  );
+
+  await expect(page.locator("c-fillinif"))
+    .toContainText(`{"showList":true,"items":["A","B","C"]}
+    item: A
+    item: B
+    item: C`);
+
+  await page.getByRole("button", { name: "Toggle List" }).click();
+  await expect(page.locator("c-fillinif")).toContainText(`{"showList":false,"items":["A","B","C"]}
+    List is hidden`);
+
+  await page.getByRole("button", { name: "Toggle List" }).click();
+  await page.getByRole("button", { name: "Add Item" }).click(); await expect(page.locator("c-fillinif"))
+  .toContainText(`{"showList":true,"items":["A","B","C","D"]}
+  item: A
+  item: B
+  item: C
+  item: D`);
+});
