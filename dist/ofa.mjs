8ba652408b19343bf051d666ffa425777280665c
Yao <kirayao@hotmail.com>
2026-01-26 18:36:19 +0800

    fix: multiple nested inner-codes caused rendering failures (#219)
    * fix(fill): 修复嵌套模板渲染时内部代码未解套的问题
    
    * test: 添加 fill-in-temp 测试用例
    
    * chore: bump version
    
    ---------
    
    Co-authored-by: huang yao <5945154+kirakiray@users.noreply.github.com>

--- dist/ofa.mjs
+++ dist/ofa.mjs
@@ -1,4 +1,4 @@
-//! ofa.js - v4.6.15 https://github.com/kirakiray/ofa.js  (c) 2018-2026 YAO
+//! ofa.js - v4.6.16 https://github.com/kirakiray/ofa.js  (c) 2018-2026 YAO
 // const error_origin = "http://127.0.0.1:5793/errors";
 const error_origin = "https://ofajs.github.io/ofa-errors/errors";
 
@@ -7012,7 +7012,7 @@ $.register({
             targetTemp,
             data.$host || data,
             i,
-            keyName
+            keyName,
           );
 
           frag.appendChild($ele.ele);
@@ -7061,7 +7061,7 @@ $.register({
       let keyVals = [];
       const refreshKeyVals = () => {
         keyVals = Array.from(children).map((e, i) =>
-          getKeyVal(e.__render_data.$data, i)
+          getKeyVal(e.__render_data.$data, i),
         );
       };
       refreshKeyVals();
@@ -7102,7 +7102,7 @@ $.register({
             targetTemp,
             data.$host || data,
             i,
-            keyName
+            keyName,
           );
 
           selfEl.insertBefore($ele.ele, children[i]);
@@ -7169,20 +7169,42 @@ const needWrapTags = ["o-fill", "o-if", "o-else-if", "o-else"];
 
 const wrapTemp = (template) => {
   const eles = Array.from(
-    template.content.querySelectorAll(needWrapTags.join(","))
+    template.content.querySelectorAll(needWrapTags.join(",")),
   );
 
   // 倒转之后性能好像好点
   // eles.reverse();
 
+  // 对已有的 inner-code 先进行解套，不然导致渲染失败
   eles.forEach((e) => {
+    while (true) {
+      const innerCodeTempEl = e.querySelector("template[inner-code]");
+
+      if (!innerCodeTempEl) {
+        break;
+      }
+
+      const childs = innerCodeTempEl.content.childNodes;
+
+      childs.forEach((child) => {
+        innerCodeTempEl.parentNode.insertBefore(
+          child.cloneNode(true),
+          innerCodeTempEl,
+        );
+      });
+
+      innerCodeTempEl.remove();
+    }
+
     const originCode = e.innerHTML;
+    console.log("originCode: ", originCode);
+    // 对已有的 inner-code 先进行解套
     e.innerHTML = `<template inner-code>${originCode}</template>`;
     wrapTemp(e.children[0]);
   });
 };
 
-const version = "ofa.js@4.6.15";
+const version = "ofa.js@4.6.16";
 $.version = version.replace("ofa.js@", "");
 
 let isDebug = false;
--- packages/ofa/fill.mjs
+++ packages/ofa/fill.mjs
@@ -60,7 +60,7 @@ $.register({
             targetTemp,
             data.$host || data,
             i,
-            keyName
+            keyName,
           );
 
           frag.appendChild($ele.ele);
@@ -109,7 +109,7 @@ $.register({
       let keyVals = [];
       const refreshKeyVals = () => {
         keyVals = Array.from(children).map((e, i) =>
-          getKeyVal(e.__render_data.$data, i)
+          getKeyVal(e.__render_data.$data, i),
         );
       };
       refreshKeyVals();
@@ -150,7 +150,7 @@ $.register({
             targetTemp,
             data.$host || data,
             i,
-            keyName
+            keyName,
           );
 
           selfEl.insertBefore($ele.ele, children[i]);
@@ -217,14 +217,36 @@ const needWrapTags = ["o-fill", "o-if", "o-else-if", "o-else"];
 
 export const wrapTemp = (template) => {
   const eles = Array.from(
-    template.content.querySelectorAll(needWrapTags.join(","))
+    template.content.querySelectorAll(needWrapTags.join(",")),
   );
 
   // 倒转之后性能好像好点
   // eles.reverse();
 
+  // 对已有的 inner-code 先进行解套，不然导致渲染失败
   eles.forEach((e) => {
+    while (true) {
+      const innerCodeTempEl = e.querySelector("template[inner-code]");
+
+      if (!innerCodeTempEl) {
+        break;
+      }
+
+      const childs = innerCodeTempEl.content.childNodes;
+
+      childs.forEach((child) => {
+        innerCodeTempEl.parentNode.insertBefore(
+          child.cloneNode(true),
+          innerCodeTempEl,
+        );
+      });
+
+      innerCodeTempEl.remove();
+    }
+
     const originCode = e.innerHTML;
+    console.log("originCode: ", originCode);
+    // 对已有的 inner-code 先进行解套
     e.innerHTML = `<template inner-code>${originCode}</template>`;
     wrapTemp(e.children[0]);
   });
