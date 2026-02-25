606a1d1f8e0e1d710e63f327c5c9e260b8f249a3
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-02-25 14:32:46 +0800

    feat(ssr): 添加页面加载失败处理和动画配置

--- test/ssr-case/node/static/app-config.js
+++ test/ssr-case/node/static/app-config.js
@@ -24,3 +24,31 @@ export const loading = () => {
 
   return loadingEl;
 };
+
+export const fail = ({ src, error }) => {
+  return `<div style="width:100%;height:100%;display:flex;justify-content:center;align-items:center;word-break:break-all;" data-testid="error-container">
+    <div style="padding:20px;text-align:center;">
+      <h3>load fail</h3>
+      <p>${error.stack || error.toString()}</p>
+      ${src}
+      <div>
+        <button on:click="back()">Back</button>
+      </div>
+    </div>
+  </div>`;
+};
+
+export const pageAnime = {
+  current: {
+    opacity: 1,
+    transform: "translate(0, 0)",
+  },
+  next: {
+    opacity: 0,
+    transform: "translate(30px, 0)",
+  },
+  previous: {
+    opacity: 0,
+    transform: "translate(-30px, 0)",
+  },
+};
