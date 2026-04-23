17798cf1248941cd6d0a149cc52720386ebfebfa
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-04-23 22:06:11 +0800

    docs: update skill

---
+++ skills/ofajs-docs/assets/06-scsr/app-config.js
@@ -0,0 +1,14 @@
+export const pageAnime = {
+  current: {
+    opacity: 1,
+    transform: "translate(0, 0)",
+  },
+  next: {
+    opacity: 0,
+    transform: "translate(30px, 0)",
+  },
+  previous: {
+    opacity: 0,
+    transform: "translate(-30px, 0)",
+  },
+};
